01788bfe8e9e58a3f960fed8bea23fff53a920d0
dammy995 <damianemerah1@gmail.com>
2023-07-10 20:50:25 +0100

    pycodestyle error fixed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""an asynchronous coroutine that takes in an integer argument (max_delay, with a default value of 10)
-named wait_random that waits for a random delay between 0 and max_delay (included and float value)
-seconds and eventually returns it."""
+"""an asynchronous coroutine that takes in an integer argument
+(max_delay, with a default value of 10) named wait_random that
+waits for a random delay between 0 and max_delay
+(included and float value)seconds and eventually returns it."""
 import asyncio
 import random
 
