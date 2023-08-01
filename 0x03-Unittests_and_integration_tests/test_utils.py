86803724906afefa8af2213cb8ff1212bf5b9aaa
dammy995 <damianemerah1@gmail.com>
2023-08-01 01:30:26 +0100

    unittest

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""Parameterize a unit test"""
+import unittest
+from unittest.mock import patch, Mock
+from typing import Dict, Tuple, Union
+from parameterized import parameterized
+
+from utils import (access_nested_map, get_json, memoize)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test the access_nested_map function"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map: Dict, path: Tuple,
+                               expected: Union[Dict, int]) -> None:
+        """Test that the method returns what it is supposed to"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(
+            self, nested_map: Dict, path: Tuple,
+            exception: Exception) -> None:
+        """Test that a KeyError is raised for the respective inputs"""
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test the get_json function"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
+        """Test that utils.get_json returns the expected result"""
+        attrs = {'json.return_value': test_payload}
+        with patch('requests.get', return_value=Mock(**attrs)) as mock_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests the `memoize` function."""
+
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+        ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+
 from typing import Generator
 
 
@@ -11,3 +12,21 @@ print(my_generator())
 # Using the type-annotated generator
 for item in my_generator():
     print(item)
+
+
+def check_arguments(arg1, arg2):
+    assert isinstance(
+        arg1, int) and arg1 > 0, "arg1 must be an integer greater than 0"
+    assert isinstance(
+        arg2, int) and arg2 > 0, "arg2 must be an integer greater than 0"
+
+    # Rest of your code here...
+
+
+# Test the function with valid arguments
+check_arguments(10, 5)
+
+# Test the function with invalid arguments (will raise AssertionError)
+check_arguments(0, 7)
+check_arguments(3.14, 8)
+check_arguments("hello", 12)
